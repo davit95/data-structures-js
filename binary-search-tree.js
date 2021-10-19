432a9d5f4e2bbcd0c1f42d0c8fb2ad42ec3f8849
davits <davit.stepanyan@optym.com>
2021-10-19 12:21:21 +0400

    added BFS for trees

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -61,4 +61,24 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  // Breadth first search
+  bfs() {
+    let currentNode = this.root;
+    let results = [];
+    let queue = [];
+    queue.push(currentNode);
+
+    while (queue.length) {
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    return results;
+  }
 }
